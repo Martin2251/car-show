d7de7fdeff92ec1fc8d8881be9000cbaef4d2b35
Martin Lowe <martinlowe673@gmail.com>
2023-09-16 19:15:22 +0100

    footer

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,11 +4,11 @@ import React from 'react'
 import Image from 'next/image'
 import { CustomButtonProps } from '@/types'
 
-const CustomButton = ({title,containerStyles,handleClick}:CustomButtonProps) => {
+const CustomButton = ({title,containerStyles,handleClick,btnType}:CustomButtonProps) => {
   return (
     <button
+    type={btnType || "button"}
     disabled={false}
-    type={"button"}
     className={`custom-btn ${containerStyles}`}
     onClick={handleClick}
     >

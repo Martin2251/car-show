74aa4aa8f6f69f9f227179ba3e276f0974ad8e86
Martin Lowe <martinlowe673@gmail.com>
2023-09-16 13:01:33 +0100

    added images and new public file

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,6 +16,11 @@ const Hero = () => {
             <p className='hero__subtitle'>stremaline your car rental expierence with out process</p>
             <CustomButton  title="Explore cars" containerStyles="bg-primary-blue text-white rounded-full mt-10"handleClick={handleScroll}/>
         </div>
+        <div className='hero__image-container'>
+            <div className='hero__image'>
+                <Image src="/hero.png" alt="hero" fill className='object-contain'/>
+             </div>
+        </div>
     </div>
   )
 }

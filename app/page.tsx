fca4377ac2bf2bba68e882e55ed52e4f0e76cbea
Martin Lowe <martinlowe673@gmail.com>
2023-09-16 19:29:36 +0100

    adding filters

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,20 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
     <Hero />
+    <div className='mt-12 padding-x padding-y max-width' id="discover">
+      <div className='home__text-container'>
+        <h1 className='text-4xl font-extrabold'>cAR cATOLOGUE</h1>
+        <p>Explore the cars you might like</p>
+      </div>
+      <div className='home__filters'>
+        <Searchbar  />
+    <div className='home__filter-container'>
+      <FilterButton title='fuel'/>
+      <FilterButton title='fuel'/>
+    </div>
+      </div>
+
+    </div>
       
     </main>
   )

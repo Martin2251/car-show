57aa73f506fbec416a4cce5d01b39cb19ef51c05
Martin Lowe <martinlowe673@gmail.com>
2023-09-17 15:27:47 +0100

    adding utils

---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e0db8e15edmshd2ee394bb7399e8p11f48fjsn41b4ece282b9',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}

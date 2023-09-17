814f82d07b5c3bb919da6b0086f703c80bdff049
Martin Lowe <martinlowe673@gmail.com>
2023-09-17 14:54:59 +0100

    better spelling

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,18 +1,26 @@
 "use client"
+import Image from 'next/image'
 import { SearchManufacturerProps } from '@/types'
-import {Comobobox, Transition} from '@headlessui/react'
+import {Combobox, Transition} from '@headlessui/react'
 import React from 'react'
 
 const SearchManufacturer = ({manufacturer,setManufacturer}:SearchManufacturerProps) => {
   return (
     <div className='search-manufacturer'>
-        <Comobobox>
+        <Combobox>
             <div className='relative w-full'>
-            <Comobobox.Button>
-                
-            </Comobobox.Button>
+            <Combobox.Button className="absolute top-[14px]">
+                <Image 
+                src="/car-logo.svg"
+                width={20}
+                height={20}
+                className='ml-4'
+                alt="car logo"
+
+                />
+            </Combobox.Button>
             </div>
-        </Comobobox>
+        </Combobox>
     </div>
   )
 }
